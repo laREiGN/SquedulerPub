dfc1a284fb58bf19727661e27aa4c4a4f199273c
gidion <gerdwin.ruit@gmail.com>
2019-10-05 21:09:15 +0200

    Asp.net Files
    all asp.net files

---
+++ Project_c_framework/Views/Home/sql.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_c_framework.Views.Home
+{
+    public class sql
+    {
+
+    }
+}
