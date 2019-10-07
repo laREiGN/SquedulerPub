0b80017de35c4efac54ba9ceb3dd7e28a4e951cb
gidion <gerdwin.ruit@gmail.com>
2019-10-07 11:52:14 +0200

    Combined html pages
    and added simple sql functions

---
+++ Project_c_framework/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Project_c_framework.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+        public string password { get; set; }
+    }
+}
--- Project_c_framework/Views/Home/sql.cs
+++ Project_c_framework/Views/Home/sql.cs
@@ -2,11 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Npgsql;
 namespace Project_c_framework.Views.Home
 {
+    
     public class sql
     {
+        private string connString = "Host=127.0.0.1;Port=5432;Username=postgres;Password=tangodelta;Database=Scheduler";
+        public bool Check_login(string name, string password)
+        {
+            //SQL Connection
+            var connString = "Host=127.0.0.1;Port=5432;Username=postgres;Password=tangodelta;Database=Scheduler";
 
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                // Insert some data
+                // Retrieve all rows
+                var cmd = new NpgsqlCommand("SELECT id, name, password FROM public.users;", conn);
+                var reader = cmd.ExecuteReader();
+                conn.Close();
+                if (reader.FieldCount >= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            //No accounts found
+            return false;
+        }
+
+
+        public bool Create_Account(String user, String password)
+        {
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                // Insert some data
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "INSERT INTO public.users(id, name, password)VALUES (" + user + "," + password + ");";
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    return true;
+                }
+
+            }
+            //Failed to create
+            return false;
+        }
     }
 }
