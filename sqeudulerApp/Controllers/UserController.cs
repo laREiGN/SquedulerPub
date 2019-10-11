d2598a932d442f4d6da8a36c23e8175f56c75f98
gidion <gerdwin.ruit@gmail.com>
2019-10-11 12:39:12 +0200

    Chanced Projects

---
+++ sqeudulerApp/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sqeudulerApp.Services;
+
+namespace sqeudulerApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUser _User;
+
+        public UserController(IUser _IUser)
+        {
+            _User = _IUser;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_User.GetUsers);
+        }
+    }
+}
