1848033666ea91446554b609aea53b588781133a
Erdem Demirkaya <erdemdemirkaya@outlook.com>
2019-10-11 13:16:15 +0200

    Added create function/basic design

--- sqeudulerApp/Controllers/UserController.cs
+++ sqeudulerApp/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using sqeudulerApp.Models;
 using sqeudulerApp.Services;
 
 namespace sqeudulerApp.Controllers
@@ -19,6 +20,22 @@ namespace sqeudulerApp.Controllers
         public IActionResult Index()
         {
             return View(_User.GetUsers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                _User.Add(model);
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
