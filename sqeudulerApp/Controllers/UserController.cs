fc044120629394091b2fd53e20266639abff5c48
hieu401 <hieu1@hotmail.com>
2019-10-27 20:32:01 +0100

    Made the login button and create button functional

--- sqeudulerApp/Controllers/UserController.cs
+++ sqeudulerApp/Controllers/UserController.cs
@@ -10,6 +10,7 @@ using System.Net.Mail;
 using System.Drawing;
 using Microsoft.Data.SqlClient;
 using static sqeudulerApp.Scripts.Extra;
+
 namespace sqeudulerApp.Controllers
 {
     public class UserController : Controller
@@ -25,7 +26,7 @@ namespace sqeudulerApp.Controllers
 
         public IActionResult Index()
         {
-            return View(_User.GetUsers);
+            return View();
         }
 
         public IActionResult Create()
@@ -42,6 +43,7 @@ namespace sqeudulerApp.Controllers
             else
             {
                 Email em = new Email();
+                // for (int i = 1; i < _User.GetUsers.Count(); i++) better solution? ~ Tom
                 for (int i = 1; i < 1000; i++)
                 {
                     User user = _User.GetUser(i);
@@ -87,12 +89,66 @@ namespace sqeudulerApp.Controllers
         [HttpPost]
         public IActionResult Create(User model)
         {
-            if (ModelState.IsValid)
+            // used for the sign up button. when you click on that button, 
+            // the button will take the input within that form in the html and create a User object out of it and pass that as argument when it calls this action
+            // since the form only asks for email and password when loggin in, the FirstName Field remains null. Tom
+            if (model.FirstName is null)
+            {
+                return View();
+            }
+            
+            else if (ModelState.IsValid)
             {
                 _User.Add(model);
                 return RedirectToAction("Index");
             }
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Login(User model)
+        {
+            string Email = model.Email;
+            string Password = model.Password;
+            if (ModelState.IsValid && Email != null && Password != null)
+            {
+                //sql query where we search for the email address in the database with the associated password. Only 1 result should be found.
+                String query = "SELECT [Email], [Password] FROM [dbo].[User] " +
+                    "WHERE [Email]='" + Email.ToString() + "' AND [Password]='" + Password.ToString() + "';";
+                //create a sql connection with the connection string
+                SqlConnection conn = new SqlConnection(strCon);
+                //create a sql command with the new sql query and the original connection string 
+                SqlCommand comm = new SqlCommand(query, conn);
+                //open the connection
+                conn.Open();
+                //úse the original sql datareader and execute the new sql command 
+                SqlDataReader sqlResultReader = comm.ExecuteReader();
+
+                // There should be 1 row and 2 columns meaning 2 fields (row * columns)
+                if (sqlResultReader.VisibleFieldCount == 2)
+                {
+                    //close sql reader
+                    sqlResultReader.Close();
+                    //close sql connection
+                    conn.Close();
+                    return RedirectToAction("ForgotPassword");
+                }
+                else
+                {
+                    sqlResultReader.Close();
+                    conn.Close();
+                    // buggy, for some reason the path changes but you get the same page. because of that the buttons don't work anymore
+                    return RedirectToAction("Index");
+                }
+            }
+            else {
+                // test, alternative attempt to try to debug
+                return RedirectToAction("Index", new { id = _User});
+            }
+            
+        }
+
     }
+
+
 }
