ac2dacfea4960239e12533551e933a337545836c
laREiGN / Girts <43932616+laREiGN@users.noreply.github.com>
2020-01-06 06:42:04 +0100

    update

---
+++ sqeudulerApp/Models/UserAvailability.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sqeudulerApp.Models
+{
+    public class UserAvailability
+    {
+        public User user { get; set; }
+        public Availability availability { get; set; }
+    }
+}
