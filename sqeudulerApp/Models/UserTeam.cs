65ff485e4e39bd1e4214b2e8e296dff3797aec69
hieu401 <hieu1@hotmail.com>
2020-01-06 06:25:36 +0100

    concurrency error fixed

--- sqeudulerApp/Models/UserTeam.cs
+++ sqeudulerApp/Models/UserTeam.cs
@@ -11,12 +11,15 @@ namespace sqeudulerApp.Models
     {
         //Tom: key should be UserID and Team right? Or else each user can only have 1 team
         [Key]
+        [ConcurrencyCheck]
         public int UserID { get; set; }
 
         //Tom: dit is de team code
         [ForeignKey("Team")]
+        [ConcurrencyCheck]
         public string Team { get; set; }
 
+        [ConcurrencyCheck]
         public string Role { get; set; }
     }
 }
